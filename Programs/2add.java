5db725eba8d3dd4ba64cd809f5c25dcc42e00719
PavithraManokaran <34641765+PavithraManokaran@users.noreply.github.com>
2018-07-21 13:21:53 +0530

    create 2add.java

---
+++ Programs/2add.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Main
+{  
+public static void main(String args[])
+{
+Scanner value=new Scanner(System.in);
+int a,b,c;
+System.out.println("Enter a value:");
+a=value.nextInt();
+System.out.println("Enter b value:");
+b=value.nextInt();
+System.out.println("Equation");
+c=a+b;
+System.out.println(a+"+"+b+"="+c);  
+}
+} 
